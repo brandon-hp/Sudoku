77397387df1a0b86f56270ca0fa18d899912b117
brandon-hp <56833876+brandon-hp@users.noreply.github.com>
2020-01-18 14:36:39 +0800

    命令行程序源代码
    命令行程序源代码

---
+++ Sudoku/sudoku1.3/main.cpp
@@ -0,0 +1,22 @@
+﻿#include<iostream>
+#include<string>
+#include<ctime>
+#include<cstdlib>
+#include<cstdio>
+#include<string>
+#include"UsingSudoku.h"
+#include"sudoku.h"
+ int main(int argc, char **argv)
+ {
+
+	 long long num= check_argument(argc, argv);
+	 if (num > 0)
+	 {
+		 CreateSudokuN(num);
+	 }
+	 else if (num == 0)
+	 {
+		 SolvSudokuN(argv[2]);
+	 }
+	 return 0;
+ }
